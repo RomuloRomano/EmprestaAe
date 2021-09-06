c2f175423f499c3d53baba4f40790dc5db900b6b
Romulo Romano <romulo.romano.keyrus@amway.com>
2021-09-06 14:32:47 -0300

    initial commit

---
+++ empresta-ae/src/main/java/com/empresta/emprestaae/EmprestaAeApplication.java
@@ -0,0 +1,13 @@
+package com.empresta.emprestaae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestaAeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestaAeApplication.class, args);
+	}
+
+}
---
+++ empresta-ae/src/test/java/com/empresta/emprestaae/EmprestaAeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresta.emprestaae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestaAeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
